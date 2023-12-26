c1b9ed10a135ed05b33f1598f6980eb1e61ee38b
ChristianArgue <christianarguello97@hotmail.com>
2023-12-25 21:31:23 -0500

    25 Dic 2023

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
     
-    src('src/scss/app.scss') // Identificar el archivo de SASS
+    src('src/scss/**/*.scss') // Identificar el archivo de SASS
+        .pipe(plumber())
         .pipe(sass()) // Compilarlo
         .pipe(dest('build/css')) // Almacenarlo en el disco duro
 
@@ -11,7 +13,7 @@ function css(done) {
 }
 
 function dev(done) {
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
     done();
 }
 
